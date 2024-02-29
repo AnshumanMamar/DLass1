a11274ba1be602beee223b55828e033867440711
anshuman mamar <anshumanmamar@anshumans-Air.iitmlan>
2024-02-29 21:44:08 +0530

    adding Q1

---
+++ Q1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.datasets import fashion_mnist 
+
+# Load the Fashion-MNIST dataset
+(x_train, y_train), (x_test,y_test) = fashion_mnist.load_data()
+
+# Define class names for reference
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+               "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+# Create a grid for plotting
+plt.figure(figsize=(10, 10))
+
+# Loop through each classsou
+for i in range(10):
+    # Find the index of the first occurrence of the class in the labels
+    index = next(j for j, label in enumerate(y_train) if label == i)
+    
+    # Plot the image
+    plt.subplot(2, 5, i + 1)
+    plt.imshow(x_train[index], cmap='gray')
+    plt.title(class_names[i])
+    plt.axis('off')
+
+# Show the plot
+plt.show()
+
